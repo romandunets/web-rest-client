2b2fc695925d163a92c89c8ef3872965df966e85
Roman Dunets <roman.dunets.email@gmail.com>
2017-12-06 12:44:40 +0200

    Implement sending post requests

--- js/requester.js
+++ js/requester.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   
-  $("form").submit(function(e) {
+  $('#request-form').submit(function(e) {
     e.preventDefault(e);
 
     var requestForm = $('#request-form');
     var method = requestForm.find('select[name="method"]').val();
     var url = requestForm.find('input[name="url"]').val();
+    var data = requestForm.find('textarea[name="data"]').val();
     var token = requestForm.find('input[name="token"]').val();
 
     console.log('Sending ' + method + ' request to ' + url);
@@ -13,6 +14,8 @@ $(document).ready(function() {
     $.ajax({
         type: method,
         url: url,
+        data: $.parseJSON(data),
+        dataType: "json",
         headers: {
           'x-access-token': token
         }
